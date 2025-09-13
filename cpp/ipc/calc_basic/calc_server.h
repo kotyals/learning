a15aad1ed59fbad7eea7b888136d7aea878ad9ed
Shivagond Kotyal <shivagond2001@gmail.com>
2025-09-12 23:12:41 -0700

    impr: improvise the code
    - separate server with its header file for
    better readability
    - in client provide menu to user to choose
    from options to call several functions

---
+++ cpp/ipc/calc_basic/calc_server.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<sdbus-c++/sdbus-c++.h>
+#include<thread>
+#include<chrono>
+
+using namespace std;
+class CalculatorService{
+    std::unique_ptr<sdbus::IConnection> connection_;
+    std::unique_ptr<sdbus::IObject> object_;
+    string busName;
+
+public:
+    CalculatorService(string busName, string ObjPath);
+    void run();
+    int add(int a, int b);
+    int sub(int a, int b);
+    int div(int a,int b);
+};
